8938bb2cf78ecc77b47cc1e97e4911450a5e52f4
lem0n4id <61219881+lem0n4id@users.noreply.github.com>
2021-08-08 04:25:13 +0530

    addition of functions and documentation in downloadsong.py
    added new functions:
    
    checkIfExists - Checks if the file is present in ./upload or not
    
    decorateFilenameValue - removes illegal symbols from the song title (eg- ' /\:*?"<>| '
    
    searchSong - Searches the url and returns title, durationSeconds, thumbnail. Returns None if invalid url
    
    other changes- Added exception handling and documenation

--- downloadsong.py
+++ downloadsong.py
@@ -1,7 +1,44 @@
 import os
 from youtube_dl import YoutubeDL
+import youtubesearchpython
+from youtubesearchpython.internal.constants import ResultMode
 
-import subprocess
+
+# import subprocess
+
+
+def checkIfExists(filename: str) -> bool:
+    '''Checks if the file is present in ./upload or not'''
+    result = [filename for root, dir, files in os.walk('./upload')
+              if filename in files]
+    if len(result) > 0:
+        return True
+    else:
+        return False
+
+
+def decorateFilenameValue(title: str) -> str:
+    '''removes illegal symbols from the song title '''
+    title = title.translate(
+        {ord(i): None for i in r'/\:*?"<>|'})
+
+    return title
+
+
+def searchSong(url: str):
+    '''Searches the url and returns title, durationSeconds, thumbnail.
+    Returns None if invalid url'''
+
+    try:
+        video = youtubesearchpython.Video.get(url, mode=ResultMode.dict)
+        title = video["title"]
+        thumbnail = video["thumbnails"][0]["url"]
+        durationMs = video["streamingData"]["formats"][0]["approxDurationMs"]
+        durationSeconds = round(int(durationMs)/1000)
+    except:
+        return None
+
+    return title, durationSeconds, thumbnail
 
 
 '''os.system('mkdir temp')
@@ -23,8 +60,8 @@ Example for using subprocess.Popen():
 '''
 
 
-def download_url(url: str) -> str:
-    """Downloads the mp3 file for the url passed and returns the filename"""
+def download_url(url: str) -> tuple:
+    """Downloads the mp3 file for the url passed and returns the filename,duration and thumbnail url"""
 
     # making a new folder and shifting the path to that path
     '''
@@ -44,9 +81,10 @@ def download_url(url: str) -> str:
     pipe = subprocess.Popen(args, shell=True, stderr=subprocess.PIPE)
     text = pipe.communicate()
     print(text)'''
-
-    os.chdir(f'./upload')
-
+    try:
+        os.chdir(f'./upload')
+    except:
+        pass
     os.system(f'mkdir temp')
 
     # basically cd to temp
@@ -58,7 +96,7 @@ def download_url(url: str) -> str:
         # check https://github.com/ytdl-org/youtube-dl/blob/master/README.md#embedding-youtube-dl and
         # https://github.com/ytdl-org/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312
 
-        # need to change ydl_opts to add select quality feature 
+        # need to change ydl_opts to add select quality feature
 
         ydl_opts = {
             "format": "bestaudio",
@@ -93,13 +131,16 @@ def download_url(url: str) -> str:
     name = [f for root, dir, filenames in os.walk(
         f"temp") for f in filenames if os.path.splitext(f)[1] == '.mp3']
 
-    title = data["title"].translate(
-        {ord(i): None for i in r'/\:*?"<>|'})  # removes illegal symbols
+    songTitle = data['title']
+    title = decorateFilenameValue(songTitle)
 
     os.rename(f"temp/{name[0]}", f'temp/{title}.mp3')
 
+    path = os.getcwd()
+    os.chdir(path)
+
     filename = title+'.mp3'
-    return filename
+    return filename, data["duration"], data["thumbnails"][0]['url']
 
 
 def delete_mp3file(file):
@@ -111,5 +152,6 @@ def delete_mp3file(file):
 
 
 if __name__ == '__main__':
-    filename = download_url('https://www.youtube.com/watch?v=LiaYDPRedWQ')
-    delete_mp3file(filename)  # Avril Lavigne - Hello Kitty.mp3
+    # filename = download_url('https://www.youtube.com/watch?v=LiaYDPRedWQ')
+    # delete_mp3file(filename)  # Avril Lavigne - Hello Kitty.mp3
+    checkIfExists('Harry Styles - Watermelon Sugar (Official Video).mp3')
